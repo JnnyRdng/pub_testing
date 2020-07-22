8cfbd7601de004c4cfb7b1941ff0d826ac245eb2
Jonny Reading <jonnyreading@gmail.com>
2020-07-22 10:44:06 +0100

    Test the pub has a name and an amount of cash in the till

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -9,3 +9,9 @@ class TestPub(unittest.TestCase):
     def test_pub_has_name(self):
         result = self.pub.name
         self.assertEqual("The Prancing Pony", result)
+
+    def test_pub_has_cash(self):
+        result = self.pub.till
+        self.assertEqual(100.00, result)
+
+    
